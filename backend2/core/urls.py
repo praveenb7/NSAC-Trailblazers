c3acca7ff34219fbd4edcfd02a7738be6e67c1aa
Sanyam <sanyam19092000@gmail.com>
2020-10-04 04:28:20 +0530

    static views

--- backend2/core/urls.py
+++ backend2/core/urls.py
@@ -5,6 +5,10 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.Home, name='home'),
-
+    path('fire-stations/', views.NearbyFirestation, name='fire-stations'),
+    path('rescue-centers/', views.NearbyRescueCenter, name='rescue-centers'),
+    path('fire-reports/', views.NearbyFireReports, name='fire-reports'),
+    path('report/', views.ReportFire, name='report'),
+    path('community/', views.CommunityForumView, name='community'),
 
 ]
--- backend2/core/views.py
+++ backend2/core/views.py
@@ -1,5 +1,5 @@
 from django.contrib.gis.geos import GEOSGeometry
-from django.contrib.gis.measure import Distance
+from django.contrib.gis.db.models.functions import Distance
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import permissions
@@ -10,10 +10,80 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from . import tasks
 
+
 # Create your views here.
 
 def Home(request):
     return render(request, "user/dashboard.html")
+
+
+def NearbyFirestation(request):
+    user = request.user
+    userprofile = models.Profile.objects.get(user=user)
+
+    firestations = models.FireStation.objects.all()
+    firestations = firestations.annotate(distance=Distance("location", userprofile.location)).order_by('distance')[0:20]
+
+    context = {
+        "user": user,
+        "profile": userprofile,
+        "firestations": firestations,
+
+    }
+    return render(request, "user/nearby_fire_stations.html", context=context)
+
+
+def NearbyRescueCenter(request):
+    user = request.user
+    userprofile = models.Profile.objects.get(user=user)
+
+    rescuecenters = models.RescueCenter.objects.all()
+    rescuecenters = rescuecenters.annotate(distance=Distance("location", userprofile.location)).order_by('distance')[0:20]
+
+    context = {
+        "user": user,
+        "profile": userprofile,
+        "rescuecenters": rescuecenters,
+
+    }
+    return render(request, "user/nearby_rescue_centers.html", context=context)
+
+
+def NearbyFireReports(request):
+    user = request.user
+    userprofile = models.Profile.objects.get(user=user)
+
+    fire_reports = models.RescueCenter.objects.all()
+    fire_reports = fire_reports.annotate(distance=Distance("location", userprofile.location)).order_by('distance')[0:20]
+
+    context = {
+        "user": user,
+        "profile": userprofile,
+        "reports": fire_reports,
+    }
+    return render(request, "user/nearby_fire_reports.html", context=context)
+
+
+def ReportFire(request):
+    user = request.user
+    userprofile = models.Profile.objects.get(user=user)
+
+    context = {
+        "user": user,
+        "profile": userprofile,
+    }
+    return render(request, "user/report_fire.html", context=context)
+
+
+def CommunityForumView(request):
+    user = request.user
+    userprofile = models.Profile.objects.get(user=user)
+
+    context = {
+        "user": user,
+        "profile": userprofile,
+    }
+    return render(request, "user/community_forum.html", context=context)
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
@@ -97,7 +167,8 @@ class RescueCenterViewSet(viewsets.ModelViewSet):
         longitude = self.request.query_params.get('longitude' or None)
         if latitude and longitude:
             ref_location = GEOSGeometry('SRID=4326;POINT(' + str(longitude) + ' ' + str(latitude) + ')')
-            rescuecenters = rescuecenters.annotate(distance=Distance("location", ref_location)).order_by('distance')[0:6]
+            rescuecenters = rescuecenters.annotate(distance=Distance("location", ref_location)).order_by('distance')[
+                            0:6]
 
         return rescuecenters
 
