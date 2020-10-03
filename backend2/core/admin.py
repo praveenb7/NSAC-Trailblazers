303227f0c1b550523fb60a5d4bb95042ef907558
Sanyam <sanyam19092000@gmail.com>
2020-10-03 20:49:43 +0530

    admin view

--- backend2/core/admin.py
+++ backend2/core/admin.py
@@ -6,9 +6,24 @@ from leaflet.admin import LeafletGeoAdmin
 class ProfileAdmin(LeafletGeoAdmin):
     pass
 
+class FireStationAdmin(LeafletGeoAdmin):
+    pass
+
+class RescueCenterAdmin(LeafletGeoAdmin):
+    pass
+
+class UserReportAdmin(LeafletGeoAdmin):
+    pass
+
+class DeviceReportAdmin(LeafletGeoAdmin):
+    pass
+
+class UserReportReviewAdmin(LeafletGeoAdmin):
+    pass
+
 admin.site.register(models.Profile, ProfileAdmin)
-admin.site.register(models.FireStation)
-admin.site.register(models.RescueCenter)
-admin.site.register(models.UserReport)
-admin.site.register(models.DeviceReports)
-admin.site.register(models.UserReportReview)
+admin.site.register(models.FireStation, FireStationAdmin)
+admin.site.register(models.RescueCenter, RescueCenterAdmin)
+admin.site.register(models.UserReport, UserReportAdmin)
+admin.site.register(models.DeviceReports, DeviceReportAdmin)
+admin.site.register(models.UserReportReview, UserReportReviewAdmin)
